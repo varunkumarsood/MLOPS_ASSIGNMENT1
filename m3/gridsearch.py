298bfbfb1795b006f376d17c8f96c043fc4ecb62
thesomu0 <somnath.mishra@realcoderz.in>
2025-01-27 21:51:49 +0530

    complete m3

---
+++ m3/gridsearch.py
@@ -0,0 +1,21 @@
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
+param_grid = {
+    'n_estimators': [10, 50, 100],
+    'max_depth': [2, 4, 5, 6, 10, 20, 30]
+}
+
+data = load_iris()
+X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.2)
+
+clf = RandomForestClassifier()
+grid_search = GridSearchCV(estimator=clf, param_grid=param_grid, cv=5, scoring='accuracy')
+grid_search.fit(X_train, y_train)
+
+print(grid_search.best_params_)
+
+best_model_gridsearch = grid_search.best_estimator_
+joblib.dump(best_model_gridsearch, 'best_model_gridsearch.pkl')
